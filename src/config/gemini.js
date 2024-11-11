dc3fb4da9caa1b1ad05a21b07c8c5596aafc427a
AyushGupte-22 <ayushgupte9@gmail.com>
2024-11-11 13:59:58 +0530

    adding project of ifsd

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  // Replace 'YOUR_API_KEY_HERE' with your actual API key
+  const apiKey = "AIzaSyDo3tElmsl35lwkLqt0rzgx9wTcW3JgkEg";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(promt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
+    });
+  
+    const result = await chatSession.sendMessage(promt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+ export default run;
+
+
